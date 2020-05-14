4b87cbf9b85b262936085e9ffe3943ff049fa209
Josipmrden <josip.mrden@gmail.com>
2020-05-14 19:45:47 +0200

    Added choice to pick between models in BenchmarkTesting.cpp

--- executables/BenchmarkTesting.cpp
+++ executables/BenchmarkTesting.cpp
@@ -17,6 +17,7 @@ TweakableParameters createTweakableParams()
 int main(int argc, char **argv)
 {
     string logDirectory = argv[2];
+    string evaluatorString = argv[3];
 
     StateP state (new State);
 
@@ -26,44 +27,59 @@ int main(int argc, char **argv)
     auto* umdmge = new UnorderedMultiDimMultiGenEval();
 
     vector<AbstractEvaluateOp*> evaluators = {sse, mte, umdie, umdmge};
+    AbstractEvaluateOp* evaluator;
 
     int noRuns = 20;
     string functionset = "+ - * / sin";
 
-    for (auto evaluator : evaluators) {
-        vector<DatasetInfo> datasets;
-        string pointsDatasetDir = "./chosen_points_datasets/";
-        string planeDatasetDir = "./chosen_plane_datasets/";
-        string datasetType = pointsDatasetDir;
-        argv[1] = "parameters.txt";
+    if (evaluatorString == "sse")
+    {
+        evaluator = sse;
+    }
+    else if (evaluatorString == "mte")
+    {
+        evaluator = mte;
+    }
+    else if (evaluatorString == "umdie")
+    {
+        evaluator = umdie;
+    }
+    else if (evaluatorString == "umdmge")
+    {
+        evaluator = umdmge;
+    }
 
-        if (evaluator->requiresPlanes)
-        {
-            datasetType = planeDatasetDir;
-        }
+    vector<DatasetInfo> datasets;
+    string pointsDatasetDir = "./chosen_points_datasets/";
+    string planeDatasetDir = "./chosen_plane_datasets/";
+    string datasetType = pointsDatasetDir;
+    argv[1] = "parameters.txt";
 
-        if (evaluator->noTrees > 1)
-        {
-            argv[1] = "multipleGenotypeParameters.txt";
-        }
+    if (evaluator->requiresPlanes)
+    {
+        datasetType = planeDatasetDir;
+    }
 
-        datasets.push_back(DatasetInfo("CIRCLE_4_0_0_100_150", datasetType + "circle_4_0_0_100_150", "X Y 4", functionset));
-        datasets.push_back(DatasetInfo("SPHERE_5_0_0_250_300", datasetType + "sphere_5_0_0_250_300", "X Y Z 5", functionset));
-        datasets.push_back(DatasetInfo("ELLIPSE_1_2_3_4_150_200", datasetType + "ellipse_1_2_3_4_150_200", "X Y 1 2 3 4", functionset));
-        datasets.push_back(DatasetInfo("HYPERBOLA_1.5_100_150", datasetType + "hyperbola_1.5_100_150", "X Y 1.5", functionset));
-        datasets.push_back(DatasetInfo("HARMOSC_50_100", datasetType + "harmosc_50_100", "X Y Z 0.1 3", functionset));
-        datasets.push_back(DatasetInfo("NONLINHARMOSC_33_38", datasetType + "nlhmo_33_38", "X Y Z 0.1 9.8", functionset));
+    if (evaluator->noTrees > 1)
+    {
+        argv[1] = "multipleGenotypeParameters.txt";
+    }
 
-        TweakableParameters tp = createTweakableParams();
+    datasets.push_back(DatasetInfo("CIRCLE_4_0_0_100_150", datasetType + "circle_4_0_0_100_150", "X Y 4", functionset));
+    datasets.push_back(DatasetInfo("SPHERE_5_0_0_250_300", datasetType + "sphere_5_0_0_250_300", "X Y Z 5", functionset));
+    datasets.push_back(DatasetInfo("ELLIPSE_1_2_3_4_150_200", datasetType + "ellipse_1_2_3_4_150_200", "X Y 1 2 3 4", functionset));
+    datasets.push_back(DatasetInfo("HYPERBOLA_1.5_100_150", datasetType + "hyperbola_1.5_100_150", "X Y 1.5", functionset));
+    datasets.push_back(DatasetInfo("HARMOSC_50_100", datasetType + "harmosc_50_100", "X Y Z 0.1 3", functionset));
+    datasets.push_back(DatasetInfo("NONLINHARMOSC_33_38", datasetType + "nlhmo_33_38", "X Y Z 0.1 9.8", functionset));
 
-        auto *instanceRunner = new InstanceRunner(noRuns, datasets, evaluator, tp, argc, argv, state,
-                                                  logDirectory);
+    TweakableParameters tp = createTweakableParams();
 
-        while (tp.hasNext(TweakableType::Population)) {
-            ParameterSet ps = tp.getNext(TweakableType::Population);
-            instanceRunner->runInstance(ps);
-        }
+    auto *instanceRunner = new InstanceRunner(noRuns, datasets, evaluator, tp, argc, argv, state,
+                                              logDirectory);
 
+    while (tp.hasNext(TweakableType::Population)) {
+        ParameterSet ps = tp.getNext(TweakableType::Population);
+        instanceRunner->runInstance(ps);
     }
 
     return 0;
